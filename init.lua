83627127fb9b7361b2885f65a33d6008614bac91
jkhsjdhjs <leon.moeller@yahoo.de>
2016-11-24 15:12:18 +0100

    removes unneccessary timesync from init and adds executes program.lua after boot

--- init.lua
+++ init.lua
@@ -1,14 +1,1 @@
-timeserver = "timeserver1.rwth-aachen.de"
-
--- sync time on startup after the esp got an ip address
-wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function()
-    sntp.sync(timeserver)
-    wifi.eventmon.unregister(wifi.eventmon.STA_GOT_IP)
-end)
-
--- timesync every hour
-tmr.alarm(0, 3600000, tmr.ALARM_AUTO, function()
-    if wifi.sta.getip() then
-        sntp.sync(timeserver)
-    end
-end)
+dofile("program.lua")
